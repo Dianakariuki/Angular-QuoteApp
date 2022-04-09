3c411dc8b6d07aa541bc12405acf1f139539f1e4
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-09 17:33:37 +0300

    solving errors and bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuoteBoxComponent } from './quote-box/quote-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    QuoteBoxComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
