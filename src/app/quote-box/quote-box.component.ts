0060212a3b07be16c991de5b50465455a079212c
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-09 21:46:00 +0300

    binds the function call

--- src/app/quote-box/quote-box.component.ts
+++ src/app/quote-box/quote-box.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-quote-box',
-  templateUrl: './quote-box.component.html',
-  styleUrls: ['./quote-box.component.css']
+  selector: "app-quote-box",
+  templateUrl: "./quote-box.component.html",
+  styleUrls: ["./quote-box.component.css"],
 })
-export class QuoteBoxComponent implements OnInit {
+export class QuoteBoxComponent {
+  @Input() author!: string;
+  @Input() quote!: string;
+  @Input() tweetURL!: string;
+  @Input() getNewQuote!: () => void;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
 }
