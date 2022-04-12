d4765a2e6a53ceb444dd664ffd41ee3032a8b55c
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-12 08:42:48 +0300

    adds the form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteBoxComponent } from './quote-box/quote-box.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteBoxComponent
+    QuoteBoxComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
